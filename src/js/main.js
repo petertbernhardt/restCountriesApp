e8bf1a704943dc587547ea1974c5b8601eba4a34
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2017-08-13 13:30:34 -0700

    Updated the code to change the data structure for translations for easier display.

--- src/js/main.js
+++ src/js/main.js
@@ -38,18 +38,33 @@ app.service('CountryService', ['$http', function($http) {
 	};
 
 	this.populateCountryInfo = function(data) {
-		var countryObjectArray = [];
+		var countryObjectArray = {};
 		var keys = Object.keys(data);
+		var countryInfoKeys = ['nativeName', 'translations', 'capital', 'population', 'flag']
 		// loop through keys
-		// grab its corresponding element
+		// grab data for nativeName, translations, capital, population, and flag
 		// create an object with the key as a name property and the element as a data property
 		keys.forEach(function (key) {
-			var element = data[key];
-			var countryInfoObject = {
-				'name': key,
-				'data': element
-			};
-			countryObjectArray.push(countryInfoObject);
+			// check if the key is in countryInfoKeys
+			if (countryInfoKeys.indexOf(key) > -1) {
+				// add it to countryObjectArray
+				if (key === 'translations') {
+					// translations are special, need to add them differently
+					var translationKeys = Object.keys(data[key]);
+					var translationsArray = [];
+					translationKeys.forEach(function(translationKey) {
+						var translation = data[key][translationKey];
+						var translationObject = {
+							'code': translationKey,
+							'translation': translation
+						};
+						translationsArray.push(translationObject);
+					});
+					countryObjectArray[key] = translationsArray;
+				} else {
+					countryObjectArray[key] = data[key];
+				}
+			}
 		});
 		return countryObjectArray;
 	};
