942c48ffc13c8557db85c7c3b80a76dca63af07e
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2017-08-13 13:06:58 -0700

    Put the input and button elements in a form so the enter key can be used. Added an error section so the user is alerted when they made a search error.

--- src/app/countryInfo.js
+++ src/app/countryInfo.js
@@ -5,18 +5,24 @@ app.component("countryInfo", {
 	var ctrl = this;
 
 	ctrl.showCountryInfo = false;
+	ctrl.showError = false;
 	ctrl.countryInfo = []; // array of objects for the country data
 
 	ctrl.search = function() {
 		// Hide country info
 		ctrl.showCountryInfo = false;
+		ctrl.showError = false;
 		// take the search box's value
 		// call CountryService.search and pass it the value
 		// when data is received, display the search results
 		CountryService.search(ctrl.input).then(function(response) {
+			// success
 			ctrl.data = response.data[0];
 			ctrl.countryInfo = CountryService.populateCountryInfo(ctrl.data);
 			ctrl.showCountryInfo = true;
+		}, function(error) {
+			// error
+			ctrl.showError = true;
 		});
 	};
 });
